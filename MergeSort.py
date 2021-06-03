69284775dad9b5524cc9273a9df54fae5300c046
dorsi-alt <orsidaniel03@gmail.com>
2021-06-02 21:12:05 -0400

    Create MergeSort.py

---
+++ MergeSort.py
@@ -0,0 +1,19 @@
+num_array = list()
+print("MERGE SORT ALGORITHM")
+num = int(input("Enter how many elements do you need to sort"))
+
+print('Enter numbers in array: ')
+
+for i in range(int(num)):
+
+    i = int(input("num :"))
+
+    num_array.append(int(i))
+
+def mergeSort(array):
+    pass
+
+
+
+
+mergeSort(num_array)
